43b15128131060a2a8d4ccbb4d654d56b43e4d85
jay <jaydeep.w@gmail.com>
2013-01-27 22:29:34 +0530

    Now showing the results with nice UI as bootstrap has been added to the project.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -27,17 +27,27 @@ function movieFinderCtrl($scope, $http) {
 function searchMovie(moviename, $scope, $http) {
 	console.log(moviename);
 
-	var url1 = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=" + API_KEY + "&q=" + moviename + "&callback=JSON_CALLBACK";
+	var APIUrl = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=" + API_KEY + "&q=" + moviename + "&callback=JSON_CALLBACK";
 
-	$http({method: 'JSONP', url: url1})
+	$http({method: 'JSONP', url: APIUrl})
 	.success(function(data, status, headers, config) {
 	  	var movieResult = data;
 	    // this callback will be called asynchronously
 	    // when the response is available
 	    // console.log('Success ');
 	    // console.log(movieResult);
-	    console.log("total: " + movieResult.movies.length);
-	    $scope.moviesList = movieResult.movies;
+	    var totalResults = movieResult.movies.length;
+	    console.log("total: " + totalResults);
+
+	    // if movies are found.
+	    // Update the UI.
+	    if( totalResults > 0 ) {
+	    	$scope.movie_results = "Found " + totalResults + " movies";
+	    	$scope.moviesList = movieResult.movies;
+	    } else {
+			$scope.movie_results = "No results found!";
+	    	$scope.moviesList = [];
+	    }
 	  })
 	.error(function(data, status, headers, config) {
 	    // called asynchronously if an error occurs
